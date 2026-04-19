9d13729c0bbdc51eab4c45344c3e9c63f6b33242
Vinicius <viniciuspereiradearaujo24@gmail.com>
2026-04-19 00:45:14 -0300

    Introdução a Java, aprendendo conceitos basicos.

---
+++ aula01_introducao/Hello.java
@@ -0,0 +1,16 @@
+// Primeiro comando em Java.
+
+public class Hello { // Hello é o nome da classe.
+// public - É para definir o acesso da classe
+// class - Está definindo uma classe, todo codigo funcional fica dentro de uma classe
+    public static void main(String[] args){
+    // puclic - permite que a JVM acesse este método de fora
+    // static - significa que o método a seguir pertence a classe, e não a um objeto específico
+    // String[] args - É um array onde o Java guarda os textos que você joga para o programa no momento em que aperta o start.
+        System.out.println("Olá, Mundo");
+        //System - É uma classe nativa do Java que contém ferramentas úteis para o sistema.
+        //out - É um Objeto dentro da classe System, representa saída padrão
+        //println - Ele escreve o texto e pula para a próxima linha.
+    }
+}
+
